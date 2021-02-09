feb81823f7f74b0fe4462b6b83032cf8ecb73b37
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-02-10 06:41:54 +0800

    - Middleware to check integrity of JSON payloads

---
+++ app/Http/Middleware/EnsurePayloadIsValidJson.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsurePayloadIsValidJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->isJson()) {
+            $content_type = empty($request->getContentType()) ? $request->getContentType() : 'null';
+            return response()->json([
+                'status' => false,
+                'error' => 'JSON expected. Got Content-Type: ' . $content_type . ' instead.'
+            ], 400);
+        }
+
+        if (!$request->json()->count()) {
+            return response()->json([
+                'status' => false,
+                'error' => 'Malformed JSON received. Please check your input'
+            ], 400);
+        }
+
+        return $next($request);
+    }
+}
