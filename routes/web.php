0fc74e2ab63f1f3a15241e57b9769c5963d317c0
Lloyd Yeo <lloydyeo@firstcom.com.sg>
2021-02-11 18:24:22 +0800

    - Added redirect to documentation

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://documenter.getpostman.com/view/14545141/TW77hiuD');
 });
